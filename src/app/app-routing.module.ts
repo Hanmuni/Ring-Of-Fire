1287cfb5102434a5c738f684f9687303cae6313d
Hong Hanh Chu <honghanhchu@Hong-Hanh-Mac-Mini-M1.local>
2023-01-16 15:58:01 +0100

    load current game from firestore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { StartScreenComponent } from './start-screen/start-screen.component';
 
 const routes: Routes = [
   {path: '', component: StartScreenComponent},
-  {path: 'gameSection', component: GameSectionComponent}
+  {path: 'gameSection/:id', component: GameSectionComponent}
 ];
 
 @NgModule({
--- src/app/game-section/game-section.component.ts
+++ src/app/game-section/game-section.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
 import { MatDialog } from '@angular/material/dialog';
 import { AddPlayerDialogComponent } from '../add-player-dialog/add-player-dialog.component';
-import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -15,27 +15,39 @@ export class GameSectionComponent implements OnInit {
   drawCardAnimation = false;
   currentCard: string = '';
   game: Game;
+  gamesId: string;
 
 
-  constructor(private firestore: AngularFirestore, public dialog: MatDialog) {
+  constructor(private route: ActivatedRoute, private firestore: AngularFirestore, public dialog: MatDialog) {
 
   }
 
   ngOnInit() {
     this.newGame();
-    this
-      .firestore
-      .collection('games')
-      .valueChanges()
-      .subscribe((game) => {
-        console.log(this.game);
-      })
+    
+    this.route.params.subscribe((params) => {
+      console.log(params['id']);
+      this.gamesId = params['id'];
+      
+      this
+        .firestore
+        .collection('games')
+        .doc(this.gamesId)
+        .valueChanges()
+        .subscribe((game: any) => {
+          console.log(this.game);
+          this.game.currentPlayer = game.currentPlayer;
+          this.game.playedCards = game.playedCards;
+          this.game.players = game.players;
+          this.game.stack = game.stack;
+        })
+    });
   }
   newGame() {
     this.game = new Game();
-    this.firestore
-      .collection('games')
-      .add(this.game.toJSON());
+    //this.firestore
+    // .collection('games')
+    //.add(this.game.toJSON());
   }
   drawCard() {
     if (this.game.players.length < 2) {

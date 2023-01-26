95c32a501922ec01c473d08e29cb3d413546aae8
Hong Hanh Chu <honghanhchu@Hong-Hanh-Mac-Mini-M1.local>
2023-01-26 16:59:01 +0100

    firebase hosting 2.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,9 @@ export const environment = {
   firebase: {
     projectId: 'ring-of-fire-52d85',
     appId: '1:551361527189:web:dfa1e2245d64f7e4f90a9c',
+    databaseURL: 'https://ring-of-fire-52d85-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'ring-of-fire-52d85.appspot.com',
+    locationId: 'europe-west',
     apiKey: 'AIzaSyDgH4pyzL0QTUTJJ7cPy1skaX2HLp1knfQ',
     authDomain: 'ring-of-fire-52d85.firebaseapp.com',
     messagingSenderId: '551361527189',
